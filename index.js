1d9212b07e358e6ec5991d9152053a38199bca01
github username <github email address>
2020-07-01 20:46:17 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Your code here
+function mapToNegativize(arr){
+  const newArr = []
+  for (let i = 0; i < arr.length; i++){
+    newArr.push(arr[i] * -1 )
+  }
+  return newArr
+}
+
+function mapToNoChange(arr){
+  return arr
+}
+
+function mapToDouble(arr){
+  const newArr = []
+  for (let i = 0; i < arr.length; i++){
+    newArr.push(arr[i] * 2 )
+  }
+  return newArr
+}
+
+function mapToSquare(arr){
+  const newArr = []
+  for (let i = 0; i < arr.length; i++){
+    newArr.push(arr[i] ** 2 )
+  }
+  return newArr
+}
+
+function reduceToTotal(arr, sum=0){
+  for (let i = 0; i < arr.length; i++){
+    sum += arr[i]
+  }
+  return sum
+}
+
+function reduceToAllTrue(arr){
+  for (let i = 0; i < arr.length; i++){
+    // if one value is falsey 
+    if (!arr[i]){
+      return false
+    }
+  }
+  // if it gets to here, they are all true
+  return true
+}
+
+function reduceToAnyTrue(arr){
+  for (let i = 0; i < arr.length; i++){
+    // if one value is truthy 
+    if (!!arr[i]){
+      return true
+    }
+  }
+  // if it gets to here, they are all false
+  return false
+}
